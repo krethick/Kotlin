8c5b7d5a9278dfc3232ce179a1505aca8b4684c9
Krethick Chakravarthy <krethick.chakravarthy@gavait.com>
2023-05-15 12:03:36 +0530

    Finished few basics concepts of kotlin

---
+++ src/MyApp.kt
@@ -0,0 +1,5 @@
+fun main(args: Array<String>){
+     println("Welcome to ChezzyCode");
+     println(1+2)
+     println(false)
+}
---
+++ src/Variable.kt
@@ -0,0 +1,14 @@
+fun main()
+{
+    var score: Int = 12
+    var temprature : Double = 89.4
+    var isRaining: Boolean = false
+    var alphabet: Char = 'A'
+    var message: String = "Hello World"
+    println(score)
+    println(temprature)
+    println(isRaining)
+    println(alphabet)
+    println(message)
+
+}
