1fc9f6d264d4280f92173d7c5e948d31b6897a0c
Krethick Chakravarthy <krethick.chakravarthy@gavait.com>
2023-05-16 11:27:11 +0530

    string templating

--- src/for_loop.kt
+++ src/for_loop.kt
@@ -1,19 +1,10 @@
 fun main()
 {
-    /* for(i in 1..5 step 2)
+    val number = 2
+    for(i in 1..10)
     {
-        println(i)
-    }
-
-    println("Until")
-    for (i in 1 until 5)
-    {
-        println(i)
-    }*/
-
-    println("Down to")
-    for (i in 10 downTo  1)
-    {
-        println(i)
+        //println(number.toString() + " x " + i + " = " + (number * i))
+        //String Templating
+        println("$number x $i = ${number * i}")
     }
 }
