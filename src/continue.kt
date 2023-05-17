f7afd7a2755b75cb6e39704a7c64753d80358ea6
Krethick Chakravarthy <krethick.chakravarthy@gavait.com>
2023-05-17 12:38:39 +0530

    added a comment

---
+++ src/continue.kt
@@ -0,0 +1,30 @@
+/*
+fun main(args: Array<String>)
+{
+    var i = 0;
+    while (i++ < 6)
+    {
+        if(i==3)
+        {
+            continue
+        }
+        println(i)
+    }
+} */
+
+// Labeled continue
+
+fun main(args: Array<String>)
+{
+    outerLoop@ for (i in 1..3)
+        innerLoop@ for (j in 1..3)
+        {
+            if(i == 2)
+            {
+                continue@outerLoop
+            }
+            println("i = $i and j = $j")
+        }
+}
+
+//Hello how are you
